cbd2fbeba196ef3a2514272f808c0458cfabc940
Oleg Akifev <Olegro1999@yandex.ru>
2024-04-02 01:45:16 +0300

    add filter by married/single

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -2,15 +2,33 @@ from django.contrib import admin, messages
 from .models import Women, Category
 
 
+class MarriedFilter(admin.SimpleListFilter):
+    title = 'Статус женщин'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('married', 'Замужем'),
+            ('single', 'Не замужем')
+        ]
+
+    def queryset(self, request, queryset=None):
+        if self.value() == 'married':
+            return queryset.filter(husband__isnull=False)
+        elif self.value() == 'single':
+            return queryset.filter(husband__isnull=True)
+
+
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
     list_display = ('title', 'time_create', 'is_published', 'category', 'brief_info')
     list_display_links = ('title',)
     ordering = ('-time_create', 'title',)
     list_editable = ('is_published',)
-    list_per_page = 4
+    list_per_page = 5
     actions = ('set_published', 'set_draft',)
-    search_fields = ('title',)
+    search_fields = ('title__startswith', 'category__name')
+    list_filter = (MarriedFilter, 'category__name', 'is_published',)
 
     @admin.display(description='Краткое описание', ordering='content')
     def brief_info(self, women: Women):
