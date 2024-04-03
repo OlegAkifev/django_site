459944f397c66c39409aaf5cb45d3900f311a9fc
Oleg Akifev <Olegro1999@yandex.ru>
2024-04-04 00:56:15 +0300

    add writing data from POST request to DataBase

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -44,6 +44,11 @@ def add_page(request):
         form = AddPostForm(request.POST)
         if form.is_valid():
             print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления поста')
     else:
         form = AddPostForm()
 
