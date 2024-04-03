a60c8c0ebf9850c33caa5740bc71f9610989fe14
Oleg Akifev <Olegro1999@yandex.ru>
2024-04-04 00:54:45 +0300

    add labels to form's fields and unique css classes

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -3,12 +3,14 @@ from .models import Category, Husband
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
+    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': 'form-input'}))
     slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField(required=False)
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
-    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False)
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label='Текст статьи')
+    is_published = forms.BooleanField(required=False, initial=True, label='Опубликовано')
+    category = forms.ModelChoiceField(queryset=Category.objects.all(),
+                                      label='Категория', empty_label='Категория не выбрана')
+    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False,
+                                     label='Муж', empty_label='Не замужем')
 
 
 
