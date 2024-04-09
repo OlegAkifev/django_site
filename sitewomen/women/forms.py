cd4898e4ec64e99c87b457fe63ba6239d8401d91
Oleg Akifev <Olegro1999@yandex.ru>
2024-04-10 01:37:18 +0300

    add UploadFileForm class for upload files

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -44,3 +44,10 @@ class AddPostForm(forms.ModelForm):
             raise ValidationError('Длина превышает 50 символов')
 
         return title
+
+
+class UploadFileForm(forms.Form):
+    file = forms.FileField(label="Файл")
+
+
+
