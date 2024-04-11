c601d03c7f5153dfa5856b020b129f7115caba3d
Oleg Akifev <Olegro1999@yandex.ru>
2024-04-11 13:21:10 +0300

    as_view()

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('login/', views.login, name='login'),
     path('post/<slug:post_slug>', views.show_post, name="post"),
-    path('category/<slug:category_slug>', views.show_category, name="category"),
-    path('tag/<slug:tag_slug>', views.show_tag_postlist, name='tag')
+    path('category/<slug:category_slug>', views.WomenCategory.as_view(), name="category"),
+    path('tag/<slug:tag_slug>', views.TagPostList.as_view(), name='tag')
 ]
