adfae35f2bc4f9029fecfe2937f9e861aa86ba81
Oleg Akifev <Olegro1999@yandex.ru>
2024-05-09 00:23:16 +0300

    add login/logout FBV

--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -1,12 +1,31 @@
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from users.forms import LoginUserForm
 
 
 def login_user(request):
-    return HttpResponse('login')
+    if request.method == 'POST':
+        form = LoginUserForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            print(cd)
+            user = authenticate(request, username=cd['user_name'],
+                                password=cd['password'])
+            if user and user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse_lazy('home'))
+    else:
+        form = LoginUserForm()
+    return render(request, 'users/login.html', {'form': form})
 
 
 def logout_user(request):
-    return HttpResponse('logout')
+    logout(request)
+
+    return HttpResponseRedirect(reverse_lazy('users:login'))
+
 
 
